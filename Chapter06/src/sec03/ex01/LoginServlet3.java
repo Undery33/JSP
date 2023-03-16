b353d6dc02fb66aeadbfb57d427101d9aaebe6f9
Undery <127064386+Undery33@users.noreply.github.com>
2023-03-16 16:00:50 +0900

    Add files via upload
    2022.03.16

---
+++ Chapter06/src/sec03/ex01/LoginServlet3.java
@@ -0,0 +1,61 @@
+package sec03.ex01;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.Servlet;
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class LoginServlet2
+ */
+@WebServlet("/Login3")
+public class LoginServlet3 extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see Servlet#init(ServletConfig)
+	 */
+	public void init(ServletConfig config) throws ServletException {
+		// TODO Auto-generated method stub
+		System.out.println("init 메소드 실행");
+	}
+
+	/**
+	 * @see Servlet#destroy()
+	 */
+	public void destroy() {
+		// TODO Auto-generated method stub
+		System.out.println("destroy 메소드 실행");
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		request.setCharacterEncoding("UTF-8");
+		response.setContentType("text/html;charset=UTF-8");
+		
+		PrintWriter out = response.getWriter();
+		
+		String user_ID = request.getParameter("user_ID");
+		String user_PW = request.getParameter("user_PW");
+		
+		String data = "<html>";
+		data += "<body>";
+		data += "아이디 : " + user_ID;
+		data += "<br>";
+		data += "비밀번호 : " + user_PW;
+		data += "</body>";
+		data += "</html>";
+		
+		out.print(data);
+	}
+
+}
---
+++ Chapter06/src/sec04/ex01/LoginServlet5.java
@@ -0,0 +1,78 @@
+package sec04.ex01;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class LoginServlet5
+ */
+@WebServlet("/Login5")
+public class LoginServlet5 extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see Servlet#init(ServletConfig)
+	 */
+	public void init(ServletConfig config) throws ServletException {
+		// TODO Auto-generated method stub
+		System.out.println("init 메소드 호출");
+	}
+
+	/**
+	 * @see Servlet#destroy()
+	 */
+	public void destroy() {
+		// TODO Auto-generated method stub
+		System.out.println("destroy 메소드 호출");
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		System.out.println("doGet 메소드 호출");
+		doHandle(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		System.out.println("doPost 메소드 호출");
+		doHandle(request, response);
+	}
+	
+	private void doHandle(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
+		System.out.println("doHandle 메소드 호출");
+		
+		request.setCharacterEncoding("UTF-8");
+		response.setContentType("text/html;charset=UTF-8");
+		
+		PrintWriter out = response.getWriter();
+		
+		String user_ID = request.getParameter("user_ID");
+		String user_PW = request.getParameter("user_PW");
+		String user_Addr = request.getParameter("user_address");
+		
+		String data = "<html>";
+		data += "<body>";
+		data += "아이디 : " + user_ID;
+		data += "<br>";
+		data += "비밀번호 : " + user_PW;
+		data += "<br>";
+		data += "주소 : " + user_Addr;
+		data += "</body>";
+		data += "</html>";
+		
+		out.print(data);
+	}
+}
