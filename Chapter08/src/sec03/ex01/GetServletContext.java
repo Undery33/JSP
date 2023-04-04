041c01df4e4c40f983941f0920730451b0452d4c
Undery <127064386+Undery33@users.noreply.github.com>
2023-04-04 13:11:03 +0900

    Add files via upload

---
+++ Chapter08/src/sec03/ex01/GetServletContext.java
@@ -0,0 +1,44 @@
+package sec03.ex01;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class GetServletContext
+ */
+@WebServlet("/cget")
+public class GetServletContext extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.setContentType("text/html;charset=utf-8");
+		
+		PrintWriter out = response.getWriter();
+		// getservletContext 객체 가져오기
+		ServletContext context = getServletContext();
+		
+		// member로 이전에 바인딩 된 회원정보 가져옴.
+		List member = (ArrayList)context.getAttribute("member");
+		String name = (String)member.get(0);
+		int age = (Integer)member.get(1);
+		
+		out.print("<html><body>");
+		out.print("이름 : " + name + "<br>");
+		out.print("나이 : " + age + "<br>");
+		out.print("</body></html>");
+	}
+
+}
