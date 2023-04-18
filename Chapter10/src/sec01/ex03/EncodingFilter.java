d9c06b3ce672de10d8e3136652d6512781118e93
Undery <127064386+Undery33@users.noreply.github.com>
2023-04-18 12:58:16 +0900

    Add files via upload

---
+++ Chapter10/src/sec01/ex03/EncodingFilter.java
@@ -0,0 +1,56 @@
+package sec01.ex03;
+
+import java.io.IOException;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpFilter;
+
+/**
+ * Servlet Filter implementation class EncodingFilter
+ */
+@WebFilter("/*")
+public class EncodingFilter extends HttpFilter implements Filter {
+      ServletContext context;
+    /**
+     * @see HttpFilter#HttpFilter()
+     */
+    public EncodingFilter() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see Filter#destroy()
+	 */
+	public void destroy() {
+		// TODO Auto-generated method stub
+		System.out.println("destroy 호출");
+	}
+
+	/**
+	 * @see Filter#doFilter(ServletRequest, ServletResponse, FilterChain)
+	 */
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+		System.out.println("doFilter 호출");
+		// 한글 인코딩 설정
+		request.setCharacterEncoding("utf-8");
+		response.setContentType("text/html;charset=utf-8");
+		
+		// 다음 필터로 넘겨주는 작업
+		chain.doFilter(request, response);
+	}
+
+	/**
+	 * @see Filter#init(FilterConfig)
+	 */
+	public void init(FilterConfig fConfig) throws ServletException {
+		// TODO Auto-generated method stub
+	}
+
+}
---
+++ Chapter10/src/sec01/ex03/LoginServlet.java
@@ -0,0 +1,40 @@
+package sec01.ex03;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class LoginServlet
+ */
+@WebServlet("/login")
+public class LoginServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// 한글 전송 시 인코딩 작업 생략 - Filter로 처리
+		// request.setCharacterEncoding("utf-8");
+		// response.setContentType("text/html;charset=utf-8");
+		PrintWriter out = response.getWriter();
+		
+		String user_name = (String)request.getParameter("user_name");
+		String user_pw = (String)request.getParameter("user_pwd");
+		
+		String data = "안녕하세요! <br>" + user_name + "님 환영합니다. <br><br>";
+		data += "<html><body>";
+		data += "아이디 : " + user_name + "<br>";
+		data += "비밀번호 : " + user_pw + "<br>";
+		data += "</body></html>";
+		out.print(data);
+		
+	}
+
+}
